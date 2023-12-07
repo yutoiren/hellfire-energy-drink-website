11bda9732e7bd98b5d36bcc3df7b3c4e100a6ad7
Lakshmanj <32884137+Lakshmanj@users.noreply.github.com>
2023-12-07 13:36:52 -0500

    confirmed order

--- html/js/orders.js
+++ html/js/orders.js
@@ -6,3 +6,20 @@ function paymentValidate() {
     }
     return true;
 }
+
+
+function orderConfirmed() {
+    const form = document.getElementById("ordering");
+    form.addEventListener("submit", (e) => {
+      e.preventDefault();
+      alert("Order confirmed, please check your email for the confirmation!");
+    });
+}
+
+function orderConfirmed() {
+    const form = document.getElementById("ordering1");
+    form.addEventListener("submit", (e) => {
+      e.preventDefault();
+      alert("Order confirmed, please check your email for the confirmation!");
+    });
+}
